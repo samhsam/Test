546c4f6b326f95d8d5786b5e856f4007e48a57d7
Sam <samhsam@gmail.com>
2017-04-05 20:18:20 +1000

    this is commit test2

--- app/src/androidTest/java/examples/test/ApplicationTest.java
+++ app/src/androidTest/java/examples/test/ApplicationTest.java
@@ -3,7 +3,7 @@ package examples.test;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
+/** this is to test push
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
